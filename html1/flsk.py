c8a71664c7d936b1b9a2c46be0f7d261364270e3
RuAnDroid <120510655+RuAnDroid@users.noreply.github.com>
2023-03-14 16:38:49 +0300

    Add files via upload

--- html1/flsk.py
+++ html1/flsk.py
@@ -1,6 +1,39 @@
-from flask import Flask, render_template, url_for, request
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from FDateBase import FDateBase
+DATABASE = "/tmp/flsk.db"
+DEBUG = True
+SECRET_KEY = '692e207b33b1b1b9166a4c444ccdaf83efe221c8'
 
 app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = 'werwerwerwerwer342345425vbnvbcvbxhkkhkiopiop7867'
+
+app.config.from_object(__name__)
+
+
+app.config.update(DATABASE=os.path.join(app.root_path, 'flsk.db'))
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
 
 menu = [{"name": "Главная", "url": "index"},
         {"name": "О нас", "url": "about"},
@@ -12,8 +45,28 @@ menu = [{"name": "Главная", "url": "index"},
 @app.route("/")
 @app.route("/index")
 def index():
-    print(url_for('index'))
-    return render_template("index.html", title="Главная", menu=menu)
+    db = get_db()
+    dbase = FDateBase(db)
+    # print(url_for('index'))
+    return render_template("index.html", title="Главная", menu=dbase.get_menu())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDateBase(db)
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'])
+            if not res:
+                flash("Ошибка добавления статьи", category='error')
+            else:
+                flash("Статья добавлена успешно", category='success')
+        else:
+            flash("Ошибка добавления статьи", category='error')
+
+    return render_template("add_post.html", title="Добавление статьи", menu=dbase.get_menu())
+
 
 
 @app.route("/about")
@@ -24,33 +77,61 @@ def about():
 
 @app.route("/news")
 def news():
-    print(url_for('news'))
-    return render_template("news.html", title="Новости", menu=menu)
-
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template("news.html", title="Новости", menu=dbase.get_menu())
 
 @app.route("/trade")
 def trade():
-    print(url_for('trade'))
-    return render_template("trade.html", title="Торговля", menu=menu)
-
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template("trade.html", title="Торговля", menu=dbase.get_menu())
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
-    if request.method == "POST":
-        print(request.form)
-        context = {
-            "username": request.form['username'],
-            "email": request.form['email'],
-            "message": request.form['message']
-        }
-        return render_template("contact.html", **context, title="Обратная связь", menu=menu)
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash("Сообщение отправлено успешно!", category='success')
+        else:
+            flash("Ошибка отправки", category='error')
+
+        # print(request.form)
+        # context = {
+        #     "username": request.form['username'],
+        #     "email": request.form['email'],
+        #     "message": request.form['message']
+        # }
+        return render_template("contact.html", title="Обратная связь", menu=menu)
 
     return render_template("contact.html", title="Обратная связь", menu=menu)
 
 
 @app.route("/profile/<username>")
 def profile(username):
-    return f"Пользователь : {username}"
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Пользователь: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "admin" and request.form['psw'] == "123456":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 if __name__ == "__main__":
