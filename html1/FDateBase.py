09585dfab8d0659a5de95d096237d791815e3fcc
RuAnDroid <120510655+RuAnDroid@users.noreply.github.com>
2023-03-21 14:12:24 +0300

    Add files via upload

--- html1/FDateBase.py
+++ html1/FDateBase.py
@@ -2,6 +2,7 @@ import sqlite3
 import time
 import math
 
+
 class FDateBase:
     def __init__(self, db):
         self.__db = db
@@ -21,12 +22,31 @@ class FDateBase:
     def add_post(self, title, text):
         try:
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO posts VALUES(NULL,?,?,?)",(title, text,tm))
+            self.__cur.execute("INSERT INTO posts VALUES(NULL,?,?,?)", (title, text, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления статьи в БД" + str(e))
             return False
         return True
-    def trade(self,title):
-        self.__cur.execute("INSERT INTO posts VALUES(NULL,?)", (title))
-        self.__db.commit()
+
+    def get_post(self, post_id):
+        try:
+            self.__cur.execute(f"SELECT title, text FROM posts WHERE id ={post_id} LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статьи из БД" + str(e))
+
+        return False, False
+
+    def get_posts_announce(self):
+        try:
+            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения статьи из БД" + str(e))
+        return []
+
--- html1/flsk.py
+++ html1/flsk.py
@@ -48,7 +48,8 @@ def index():
     db = get_db()
     dbase = FDateBase(db)
     # print(url_for('index'))
-    return render_template("index.html", title="Главная", menu=dbase.get_menu())
+    return render_template("index.html", title="Главная", menu=dbase.get_menu(),
+                           posts = dbase.get_posts_announce())
 
 
 @app.route("/add_post", methods=["POST", "GET"])
@@ -68,18 +69,22 @@ def add_post():
     return render_template("add_post.html", title="Добавление статьи", menu=dbase.get_menu())
 
 
+@app.route("/post/<int:id_post>")
+def show_post(id_post):
+    db = get_db()
+    dbase = FDateBase(db)
+    title, post = dbase.get_post(id_post)
+    if not title:
+        abort(404)
+    return render_template("post.html", title="title", post=post, menu=dbase.get_menu())
+
+
 
 @app.route("/about")
 def about():
     print(url_for('about'))
     return render_template("about.html", title="О нас", menu=menu)
 
-
-@app.route("/news")
-def news():
-    db = get_db()
-    dbase = FDateBase(db)
-    return render_template("news.html", title="Новости", menu=dbase.get_menu())
 
 @app.route("/trade")
 def trade():
