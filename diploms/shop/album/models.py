d6b7c727db98e401e4fd6f5fae99f5eaca3fd02d
RuAnDroid <120510655+RuAnDroid@users.noreply.github.com>
2023-04-18 16:34:40 +0300

    add commit

--- diploms/shop/album/models.py
+++ diploms/shop/album/models.py
@@ -5,6 +5,7 @@ class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     featured_image = models.ImageField(upload_to='projects/%Y/%m/%d/', default="default.jpg")
+    seller = models.CharField(max_length=20)
     price = models.CharField(max_length=10)
 
     def __str__(self):
